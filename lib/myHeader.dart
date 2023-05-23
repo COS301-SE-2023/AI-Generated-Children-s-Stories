00c96fe16e1678e51093b6bd1ac2e56e28fe0692
thomasisebeck <thomasisebeck@gmail.com>
2023-05-23 14:42:58 +0200

    added header

--- lib/myHeader.dart
+++ lib/myHeader.dart
@@ -15,108 +15,68 @@ class MyHeader extends StatefulWidget {
 class _MyHeaderState extends State<MyHeader> {
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Row(children: [
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.width * 0.1,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage('assets/images/Wave.png'),
-            ),
-          ),
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(width: 6, color: Color.fromARGB(255, 255, 255, 255)),
         ),
-      ],),
-      Row(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.05),
-            child: Transform.rotate(
-              angle: -0.2, 
-              child: Image.asset(
-                'assets/images/MascotWinking.png',
-                width: MediaQuery.of(context).size.width*0.15,
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.06),
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width*0.5,
-              child: 
-                Text(
-                '${widget.message}',
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 25,
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  fontWeight: FontWeight.w600
-                ),
-              ),
-            ),
-          ),
-          Stack(
-            children: [
-              Positioned(
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 5), 
-                  child:  Image.asset(
-                    'assets/images/Profile.png',
-                    width:  MediaQuery.of(context).size.width*0.2,
-                  ),
-                )
-              ),
-            ]
-          )
-          
-        ]
+           
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            //rgb(255, 243, 233)
+            Color.fromARGB(255, 255, 243, 233),
+            Color.fromARGB(255, 255, 243, 233),
+            Color.fromARGB(255, 255, 214, 180),
+          ],
+        ), 
+        
       ),
-    ]);
-  }
-}
-
-/*
-Row(children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.width * 0.1,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage('assets/images/Wave.png'),
+      child: Column(
+        children: [
+        Row(children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.width * 0.1,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage('assets/images/Wave.png'),
+              ),
+            ),
+          ),
+        ],),
+        Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.05),
+              child: Transform.rotate(
+                angle: -0.2, 
+                child: Image.asset(
+                  'assets/images/MascotWinking.png',
+                  width: MediaQuery.of(context).size.width*0.15,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.06),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width*0.5,
+                child: 
+                  Text(
+                  '${widget.message}',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 25,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontWeight: FontWeight.w600
                   ),
                 ),
               ),
-            ],),
-            Row(
+            ),
+            Stack(
               children: [
-                Padding(
-                  padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.05),
-                  child: Transform.rotate(
-                    angle: -0.2, 
-                    child: Image.asset(
-                      'assets/images/MascotWinking.png',
-                      width: MediaQuery.of(context).size.width*0.15,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.06),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width*0.5,
-                    child: 
-                      const Text(
-                      'Here\'s a new book!',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 25,
-                        color: Color.fromARGB(255, 0, 0, 0),
-                        fontWeight: FontWeight.w600
-                      ),
-                    ),
-                  ),
-                ),
                 Positioned(
                   child: Padding(
                     padding: const EdgeInsets.only(right: 5), 
@@ -125,8 +85,13 @@ Row(children: [
                       width:  MediaQuery.of(context).size.width*0.2,
                     ),
                   )
-                )
-                
+                ),
               ]
-            ),
-*/
+            )
+            
+          ]
+        ),
+      ]),
+    );
+  }
+}
