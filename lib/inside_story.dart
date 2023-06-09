77c91bcb0d9a34c354bf2d0f89a4c6395d95c2b0
thomasisebeck <thomasisebeck@gmail.com>
2023-06-09 11:20:01 +0200

    started refactoring inside a story to include the dynamic header

--- lib/inside_story.dart
+++ lib/inside_story.dart
@@ -7,27 +7,21 @@ import 'navbar.dart';
 import 'myHeader.dart';
 
 class InsideStory extends StatefulWidget {
-  InsideStory({
-    super.key
-  });
+  InsideStory({super.key});
 
-  List<String> messages = [
-    "Well done!", 
-    "Almost there!",
-    "Way to go!"
-  ];
+  List<String> messages = ["Well done!", "Almost there!", "Way to go!"];
 
   List<String> storyText = [
-    "Once upon a time, there was a curious ant named Andy. He lived in a cozy anthill under a big oak tree. Andy loved exploring with his ant friends every day.", 
-    "One sunny morning, Andy found a shiny golden key. He didn't know where it came from or what it unloc, ked, but he was determined to find out.", 
+    "Once upon a time, there was a curious ant named Andy. He lived in a cozy anthill under a big oak tree. Andy loved exploring with his ant friends every day.",
+    "One sunny morning, Andy found a shiny golden key. He didn't know where it came from or what it unloc, ked, but he was determined to find out.",
     "Carrying the key, Andy went on an exciting journey through tall grass, up branches, and across a tiny stream. Finally, he discovered a huge rock with door.",
     "Andy turned the key in the keyhole, and the door opened, revealing a hidden chamber. Inside, there was delicious food."
   ];
 
   List<String> images = [
-    "assets/images/stories/AndyTheAnt/img1.jpg", 
-    "assets/images/stories/AndyTheAnt/img2.jpg", 
-    "assets/images/stories/AndyTheAnt/img3.jpg", 
+    "assets/images/stories/AndyTheAnt/img1.jpg",
+    "assets/images/stories/AndyTheAnt/img2.jpg",
+    "assets/images/stories/AndyTheAnt/img3.jpg",
     "assets/images/stories/AndyTheAnt/img4.jpg"
   ];
 
@@ -36,9 +30,8 @@ class InsideStory extends StatefulWidget {
 }
 
 class _InsideStoryState extends State<InsideStory> {
-
-  int messageIndex = 0; 
-  int storyIndex = 0; 
+  int messageIndex = 0;
+  int storyIndex = 0;
 
   void next() {
     if (storyIndex < widget.storyText.length - 1) {
@@ -61,168 +54,66 @@ class _InsideStoryState extends State<InsideStory> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 255, 243, 233),
-        body: SafeArea(
-          //back button
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Column(
+      backgroundColor: const Color.fromARGB(255, 255, 243, 233),
+      body: SafeArea(
+          child: Column(
+        children: [
+          MyHeader(message: widget.messages[messageIndex]),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(
+                  top: 10.0,
+                  left: MediaQuery.of(context).size.width * 0.05,
+                  right: MediaQuery.of(context).size.width * 0.05),
+              child: Row(
+                //image
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: Column(
                       children: [
-                      Row(children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8.0),
-                          child: Container(
-                            width: MediaQuery.of(context).size.width,
-                            height: MediaQuery.of(context).size.width * 0.1,
-                            decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                fit: BoxFit.fill,
-                                image: AssetImage('assets/images/Wave.png'),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],),
-                      Row(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.05),
-                            child: Image.asset(
-                                  'assets/images/heart-empty.png',
-                                  width: MediaQuery.of(context).size.width*0.12,
-                                ),
-                            ),
-                          Padding(
-                            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.06),
-                            child: SizedBox(
-                              width: MediaQuery.of(context).size.width*0.5,
-                              child: 
-                                Text(
-                                widget.messages[messageIndex],
-                                textAlign: TextAlign.center,
-                                style: const TextStyle(
-                                  fontSize: 25,
-                                  color: Color.fromARGB(255, 0, 0, 0),
-                                  fontWeight: FontWeight.w600
-                                ),
-                              ),
-                            ),
-                          ),
-                          Stack(
-                            children: [
-                              Positioned(
-                                child: Padding(
-                                  padding: const EdgeInsets.only(right: 5), 
-                                  child:  Image.asset(
-                                    'assets/images/MascotWinking.png',
-                                    width:  MediaQuery.of(context).size.width*0.2,
-                                  ),
-                                )
-                              ),
-                            ]
-                          )
-                          
-                        ]
-                      ),
-                      Row(children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 15, bottom: 5), 
-                          child: ProgressBar(totalPages: widget.storyText.length - 1, currentPages: storyIndex)
-                        )
-                        
-                      ],)
-                    ])]
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Row(
-                    //image
-                    children: [
-                        SizedBox(
-                        width: MediaQuery.of(context).size.width,
-                        child: Column(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Image.asset(
-                                widget.images[storyIndex],
-                              ),
-                            )
-                            
-                          ],
-                        ),
-                      ),
-                    ],                
-                  ),
-                ), 
-                SingleChildScrollView(
-                  child: Column(children: [
-                    Padding( //story text
-                      padding: const EdgeInsets.all(20.0),
-                      child:Text(
-                        widget.storyText[storyIndex], 
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontSize: 25
-                        )
-                      )                
-                    ), 
-                  ],) 
-                ),
-            
-                Column(
-              children: [
-                Padding(
-                      padding: EdgeInsets.only(bottom: 20, top: 15, left: MediaQuery.of(context).size.width * 0.05, right: MediaQuery.of(context).size.width * 0.05),
-                      child: Row(children: [
-                        GestureDetector(
-                          onTap: () => {
-                            prev()
-                          },
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
                           child: Image.asset(
-                            'assets/images/back.png'
+                            widget.images[storyIndex],
                           ),
-                        ),
-                         
-                        Spacer(),
-                        Image.asset(
-                          'assets/images/pause.png'
-                        ),
-                        Spacer(), 
-          
-                        GestureDetector(
-                          onTap: () => {
-                            next()
-                          },
-                          child: Image.asset(
-                            'assets/images/forward.png'
-                          )
                         )
-                        ,
-                      ],),
+                      ],
                     ),
-              ],
-            )
-              ]
+                  ),
+                ],
+              ),
             ),
-            
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                bottom: 20,
+                top: 15,
+                left: MediaQuery.of(context).size.width * 0.05,
+                right: MediaQuery.of(context).size.width * 0.05),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () => {prev()},
+                  child: Image.asset('assets/images/back.png'),
+                ),
+                Spacer(),
+                Image.asset('assets/images/pause.png'),
+                Spacer(),
+                GestureDetector(
+                    onTap: () => {next()},
+                    child: Image.asset('assets/images/forward.png')),
+              ],
+            ),
           )
-        ), 
-
-        bottomNavigationBar: NavbarWidget(),
-       
+        ],
+      )),
+      bottomNavigationBar: NavbarWidget(),
     );
-
-    
   }
 }
 
-class PageState extends ChangeNotifier {
-
-}
+class PageState extends ChangeNotifier {}
 
 /*
 
--- lib/myHeader.dart
+++ lib/myHeader.dart
@@ -15,83 +15,64 @@ class MyHeader extends StatefulWidget {
 class _MyHeaderState extends State<MyHeader> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        // border: Border(
-        //   bottom: BorderSide(width: 6, color: Color.fromARGB(255, 255, 255, 255)),
-        // ),
-           
-        // gradient: LinearGradient(
-        //   begin: Alignment.topCenter,
-        //   end: Alignment.bottomCenter,
-        //   colors: [
-        //     //rgb(255, 243, 233)
-        //     Color.fromARGB(255, 255, 243, 233),
-        //     Color.fromARGB(255, 255, 243, 233),
-        //     Color.fromARGB(255, 255, 214, 180),
-        //   ],
-        // ), 
-        
-      ),
-      child: Column(
-        children: [
-        Row(children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width * 0.1,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage('assets/images/Wave.png'),
-                ),
+    return Column(
+      children: [
+      Row(children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.width * 0.1,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage('assets/images/Wave.png'),
               ),
             ),
           ),
-        ],),
-        Row(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.05),
-              child: Transform.rotate(
-                angle: -0.2, 
-                child: Image.asset(
-                  'assets/images/MascotWinking.png',
-                  width: MediaQuery.of(context).size.width*0.15,
-                ),
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width*0.5,
-              child: 
-                Text(
-                '${widget.message}',
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 25,
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  fontWeight: FontWeight.w600
-                ),
-              ),
-            ),
-            Stack(
-              children: [
-                Positioned(
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 5), 
-                    child:  Image.asset(
-                      'assets/images/Profile.png',
-                      width:  MediaQuery.of(context).size.width*0.2,
-                    ),
-                  )
-                ),
-              ]
-            )
-            
-          ]
         ),
-      ]),
-    );
+      ],),
+      Row(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.05),
+            child: Transform.rotate(
+              angle: -0.2, 
+              child: Image.asset(
+                'assets/images/MascotWinking.png',
+                width: MediaQuery.of(context).size.width*0.15,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width*0.5,
+            child: 
+              Text(
+              '${widget.message}',
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 25,
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.w600
+              ),
+            ),
+          ),
+          Stack(
+            children: [
+              Positioned(
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 5), 
+                  child:  Image.asset(
+                    'assets/images/Profile.png',
+                    width:  MediaQuery.of(context).size.width*0.2,
+                  ),
+                )
+              ),
+            ]
+          )
+          
+        ]
+      ),
+    ]);
   }
 }
