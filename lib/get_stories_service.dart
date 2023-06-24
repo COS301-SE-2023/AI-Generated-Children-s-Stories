f1d6201b4ddfdf22b41d531b0e810855aa7fad9b
thomasisebeck <thomasisebeck@gmail.com>
2023-06-24 10:39:06 +0200

    added a comment

--- lib/get_stories_service.dart
+++ lib/get_stories_service.dart
@@ -6,26 +6,25 @@ import 'package:flutter/services.dart';
 import 'story.dart';
 
 class GetStoriesService {
+  //read the json file and reaturn the stories (simulates an api call)
+  //continue with integration tests
 
-    //read the json file and reaturn the stories (simulates an api call)
-    Future<List<Story>> fetchStories() async {
-        await Future.delayed(const Duration(seconds: 1));
-        final String response = await rootBundle.loadString('assets/stories.json');
-        final data = await json.decode(response);
+  Future<List<Story>> fetchStories() async {
+    await Future.delayed(const Duration(seconds: 1));
+    final String response = await rootBundle.loadString('assets/stories.json');
+    final data = await json.decode(response);
 
-        List<Story> stories = [];
+    List<Story> stories = [];
 
-        for (var i = 0; i < data.length; i++) {
-            stories.add(
-                Story(
-                    title: data[i]['title'],
-                    coverUrl: data[i]['coverUrl'],
-                    textContent: data[i]['textContent'].cast<String>(),
-                    imageContent: data[i]['imageContent'].cast<String>(),
-                )
-            );
-        }
-
-        return stories;
+    for (var i = 0; i < data.length; i++) {
+      stories.add(Story(
+        title: data[i]['title'],
+        coverUrl: data[i]['coverUrl'],
+        textContent: data[i]['textContent'].cast<String>(),
+        imageContent: data[i]['imageContent'].cast<String>(),
+      ));
     }
+
+    return stories;
+  }
 }
