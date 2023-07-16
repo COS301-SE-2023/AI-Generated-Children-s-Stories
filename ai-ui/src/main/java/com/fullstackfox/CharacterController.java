d2c6be0bc29c531bae9e6234e76007f8ff6c79f1
TashUni <u21526053@tuks.co.za>
2023-07-16 13:06:18 +0200

    added character generation page

---
+++ ai-ui/src/main/java/com/fullstackfox/CharacterController.java
@@ -0,0 +1,17 @@
+package com.fullstackfox;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class CharacterController {
+
+    @FXML
+    private void switchToHome() throws IOException {
+        App.setRoot("home");
+    }
+
+    @FXML
+    private void switchToTrailer() throws IOException {
+        App.setRoot("trailer-image");
+    }
+}
