bb9d0f75ca6a695438527b24d4a46e0f95ad7f1d
thomasisebeck <thomasisebeck@gmail.com>
2023-08-11 17:41:38 +0200

    fix import

--- ai-ui/src/main/java/com/fullstackfox/SettingsController.java
+++ ai-ui/src/main/java/com/fullstackfox/SettingsController.java
@@ -7,7 +7,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
-//import org.json.*;
+import org.json.*;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
--- ai-ui/src/main/java/module-info.java
+++ ai-ui/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.fullstackfox {
     requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.json;
 
     opens com.fullstackfox to javafx.fxml;
     exports com.fullstackfox;
