67cdc5564c1f5852abb48b6a75988ad24f16fdd5
TashUni <u21526053@tuks.co.za>
2023-08-11 22:20:04 +0200

    config editor complete

--- ai-ui/src/main/java/com/fullstackfox/SettingsController.java
+++ ai-ui/src/main/java/com/fullstackfox/SettingsController.java
@@ -45,32 +45,46 @@ public class SettingsController {
 
      @FXML
     private void setChannelID() throws IOException {
-     
+     if(channelID.getText() != null && !channelID.getText().isEmpty()){
+           updateJson( "channelID", channelID.getText()); 
+        }
     }
 
      @FXML
     private void setGuildID() throws IOException {
-        
+        if(guildID.getText() != null && !guildID.getText().isEmpty()){
+            updateJson( "guildID", guildID.getText()); 
+         }
     }
      @FXML
     private void setSessionID() throws IOException {
-        
+        if(sessionID.getText() != null && !sessionID.getText().isEmpty()){
+            updateJson( "sessionID", sessionID.getText()); 
+         }
     }
      @FXML
     private void setBotAuth() throws IOException {
-        
+        if(botAuth.getText() != null && !botAuth.getText().isEmpty()){
+            updateJson( "botAuthorization", botAuth.getText()); 
+         }
     }
  @FXML
     private void setUserAuth() throws IOException {
-        
+        if(userAuth.getText() != null && !userAuth.getText().isEmpty()){
+            updateJson( "userAuthorization", userAuth.getText()); 
+         }
     }
      @FXML
     private void setChatKey() throws IOException {
-        
+        if(chatKey.getText() != null && !chatKey.getText().isEmpty()){
+            updateJson( "chatGPTKey", chatKey.getText()); 
+         }
     }
      @FXML
     private void setMidSeed() throws IOException {
-        
+        if(midSeed.getText() != null && !midSeed.getText().isEmpty()){
+            updateJson( "midjourneySeed", midSeed.getText()); 
+         }
     }
 // helper function
 public void updateJson(String keyToUpdate, String newValue) {
@@ -81,11 +95,9 @@ public void updateJson(String keyToUpdate, String newValue) {
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
                 JSONTokener tokener = new JSONTokener(reader);
                 JSONObject jsonObject = new JSONObject(tokener);
-
                 // Update the specific key with the new value
                 jsonObject.put(keyToUpdate, newValue);
-
-                // Get the absolute path to the resource
+                
                 String resourceAbsolutePath = SettingsController.class.getResource(resourcesPath).getPath();
 
                 // Write the updated JSON back to the file
