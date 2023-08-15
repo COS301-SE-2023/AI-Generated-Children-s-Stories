5ae4cb6f23b327ad5639d0b34230246f5dace546
TashUni <u21526053@tuks.co.za>
2023-08-15 19:02:24 +0200

    Added image prompt page

---
+++ ai-ui/src/main/java/com/fullstackfox/ImagePromptEditorController.java
@@ -0,0 +1,34 @@
+package com.fullstackfox;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.*;
+
+
+public class ImagePromptEditorController {
+    
+    //private Button generationButton;
+    @FXML
+    private TextArea storyPrompt;
+    @FXML
+    private TextField age;
+    @FXML
+    private Slider lenSlider;
+    @FXML
+    private TextArea storyOut;
+
+    @FXML
+    private void switchToHome() throws IOException {
+        App.setRoot("home");
+    }
+    
+
+        @FXML
+    private void getPrompt() throws IOException{
+
+    }
+
+
+
+}
