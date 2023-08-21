955c8cb172f13a22467ca300687dd89261669e23
Oliver Welsh <104843826+ogb-Welsh@users.noreply.github.com>
2023-08-21 19:05:23 +0200

    fixed pom

--- ai-ui/src/main/java/module-info.java
+++ ai-ui/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.fullstackfox {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.json;
+    requires okhttp3;
 
     opens com.fullstackfox to javafx.fxml;
     exports com.fullstackfox;
