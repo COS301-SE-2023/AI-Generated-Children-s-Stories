2f75967a307aa35c67eca31dcf7e8acf883125ea
thomasisebeck <thomasisebeck@gmail.com>
2023-09-27 15:22:52 +0200

    fixed tests

--- ai-story-generation-app/src/test/java/fullstack_fox/StoryTest.java
+++ ai-story-generation-app/src/test/java/fullstack_fox/StoryTest.java
@@ -8,10 +8,16 @@ class StoryTest {
 
     @Test
     void addPage() {
+        Story story = new Story("Title", "tailer");
+        story.addPage(new Page("content", "image"));
 
+        assertEquals(story.pages.size(), 1);
     }
 
     @Test
     void printStory() {
+        Story story = new Story("Title", "tailer");
+        story.addPage(new Page("content", "image"));
+        story.printStory();
     }
 }
