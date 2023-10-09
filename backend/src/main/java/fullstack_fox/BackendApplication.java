76da485e99cec029103614b1aeb2e232f011ad45
Oliver Welsh <104843826+ogb-Welsh@users.noreply.github.com>
2023-10-09 03:22:25 +0200

    instructions

--- backend/src/main/java/fullstack_fox/BackendApplication.java
+++ backend/src/main/java/fullstack_fox/BackendApplication.java
@@ -12,6 +12,8 @@ To make jar
 1. mvn clean package -DskipTests
 2. cd target
 2. java -jar .\target\backend-1.0-SNAPSHOT.jar
+
+To copy to ec2 server scp -i fsf-ssh-key.pem backend-0.0.1-SNAPSHOT.jar ubuntu@ec2-13-245-255-235.af-south-1.compute.amazonaws.com:/home/ubuntu
 */
 
 @SpringBootApplication
