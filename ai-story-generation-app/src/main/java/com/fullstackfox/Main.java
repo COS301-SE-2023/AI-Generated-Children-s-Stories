494a0f6f942319da973f7a75fe0c221aad1fa706
Oliver Welsh <104843826+ogb-Welsh@users.noreply.github.com>
2023-10-14 23:47:53 +0200

    Added main

---
+++ ai-story-generation-app/src/main/java/com/fullstackfox/Main.java
@@ -0,0 +1,9 @@
+package com.fullstackfox;
+
+import java.net.URISyntaxException;
+
+public class Main {
+    public static void main(String[] args) throws URISyntaxException {
+        App.main(args);
+    }
+}
